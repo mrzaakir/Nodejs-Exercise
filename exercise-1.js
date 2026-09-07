68b7348d8f3815f754779ca544f77ed36d5fe068
Zakaria Kalid Adan <mrzaakiraj@gmail.com>
2026-09-07 17:20:35 +0300

    Change server port from 3010 to 3011

--- exercise-1.js
+++ exercise-1.js
@@ -74,5 +74,5 @@ app.delete("/books/:id", (req, res) => {
 });
 
 app.listen(3010, () => {
-  console.log(`Server is running at http://localhost:3010`);
+  console.log(`Server is running at http://localhost:3011`);
 });
