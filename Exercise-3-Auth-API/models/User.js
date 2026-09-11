ea921bf12815330a516380508d44ea33c39a3796
mrzaakir <mrzaakiraj@gmail.com>
2026-09-11 16:39:29 +0300

    modified:   Exercise-3-Auth-API/models/User.js

--- Exercise-3-Auth-API/models/User.js
+++ Exercise-3-Auth-API/models/User.js
@@ -26,6 +26,10 @@ const userSchema = new mongoose.Schema(
 			type: String,
 			enum: ["user", "admin"],
 			default: "user"
+		},
+		profilePicture: {
+			type: String,
+			default: null
 		}
 	},
 	{ timestamps: true }
